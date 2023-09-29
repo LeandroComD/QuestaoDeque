7ef6caf2adeb6c86013be992011b92efc44ad586
Leandro Sales Santos <68706685+LeandroComD@users.noreply.github.com>
2023-09-29 20:21:34 -0300

    Add files via upload

---
+++ Pratica4/src/br/com/questao2/Dados.java
@@ -0,0 +1,11 @@
+package br.com.questao2;
+
+public class Dados {
+    String nome, telefone, email;
+
+    public Dados(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+}
